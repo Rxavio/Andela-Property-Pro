6160cdde8f1e70d047b0e2ea39c15558297a6914
xavioo <rucahoxavi@gmail.com>
2019-07-29 17:32:17 +0200

    feature: user can search by  type [finish #167251331]

--- server/model/user-data.js
+++ server/model/user-data.js
@@ -1,16 +1,14 @@
-const users = [
-    {
-      id: 1, 
-      email: 'rx@gmail.com',
-       first_name: 'rx',
-      last_name: 'rxo ', 
-     password: '12345', 
-     phoneNumber: '078966666',
-     address: 'remera',
-     is_admin: false,
-    },
+const user_data = [
+  {
+    id: 1, email: 'rxavio@gmail.com', first_name: 'xavier', last_name: 'rucahobatinya', password: '12345', phoneNumber: '+250785209432', address: 'kigali', is_admin: true,
+  },
+  {
+    id: 2, email: 'ukureryan@gmail.com', first_name: 'Ryan', last_name: 'ukure', password: '12345', phoneNumber: '+250782200000', address: 'huye', is_admin: false,
+  },
+  {
+    id: 3, email: 'kamarivy@gmail.com', first_name: 'rivy', last_name: 'kama', password: '12345', phoneNumber: '+250788950000', address: 'muhanga', is_admin: false,
+  },
    
-    
   ];
   
-  export default users;
+  export default user_data;
